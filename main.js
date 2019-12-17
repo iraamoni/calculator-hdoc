0c3b828915715d31fb8cff0897c7b47d9143776a
Suborna Jahan <suboorna@gmail.com>
2019-12-17 23:12:18 +0100

    buttons are pretty much functional and clickable

--- main.js
+++ main.js
@@ -1,69 +1,60 @@
-function takeInput () {
-    function attachClickListener(numberElement) {
-        numberElement.addEventListener("click", function () {
-            const currentOutput = getCurrentOutput();
-            printOutput(currentOutput + numberElement.innerText);
-        });
-    };
 
-    document.querySelectorAll(".number").forEach(attachClickListener);
+function getHistory() {
+   return document.getElementById('history-value').innerText
 }
 
-function printOutput (output) {
-    document.querySelector("#output").innerText=output;
+function printHistory(num) {
+    document.getElementById('history-value').innerText=num;
 }
 
-function getCurrentOutput () {
-    return document.querySelector("#output").innerText;
+// printHistory('99')
+function getOutput() {
+    return document.getElementById('output-value').innerText;
 }
 
-takeInput();
+function printOutput(num) {
+    if (num === '') {
+        document.getElementById('output-value').innerText=num;
+    } else {
+        document.getElementById('output-value').innerText=getFormattedNumber(num);
+    }
+}
 
-// function getHistory() {
-//     return document.getElementById("history-value").innerText
-// }
+function getFormattedNumber(num) {
+    let n = Number(num)
+    let value = n.toLocaleString('en')
+    return value
+}
 
-// function printHistory(num) {
-//     document.getElementById("history-value").innerText=num;
-// }
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g,''));
+}
 
-// function getOutput() {
-//     return document.getElementById("output-value").innerText;
-// }
+const operators = document.getElementsByClassName('operator');
+for(let i=0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function() {
+        if (this.id==='clear') {
+            printHistory('')
+            printOutput('')
+        }
+        if (this.id==='backspace') {
+            let removePreviousNumber = reverseNumberFormat(getOutput()).toString();
+            if (removePreviousNumber) {
+                removePreviousNumber = removePreviousNumber.substr(0,removePreviousNumber.length-1)
+                printOutput(removePreviousNumber)
+            }
+        }
+    })
+}
 
-// function printOutput(num) {
-//     if (num == "") {
-//         document.getElementById("output-value").innerText=num;
-//     } else {
-//         document.getElementById("output-value").innerText=getFormattedNumber(num);
-//     }
-// }
-
-// function getFormattedNumber(num) {
-//     var n = Number(num);
-//     var value = n.toLocaleString("en");
-//     return value;
-// }
-
-// function reverseNumberFormat (num) {
-//     return Number(num.replace(/,/g,""));
-// }
-
-// var operator = document.getElementsByClassName("operator");
-// for (var i=0; i<operator.length; i++) {
-//     operator[i].addEventListener('click', function() {
-//     })
-// }
-
-// var number = document.getElementsByClassName("number");
-// for (var i=0; i<number.length; i++) {
-//     number[i].addEventListener('click', function() {
-//         var output=reverseNumberFormat(getOutput());
-//         if (output!=NaN) { //if output is a number
-//             output=output+this.id;
-//             printOutput(output);
-//         }
-//     });
-// }
-
+const numbers = document.getElementsByClassName('number');
+for (let i=0; i< numbers.length; i++) {
+    numbers[i].addEventListener('click', function() {
+        let consecutiveNumbers = reverseNumberFormat(getOutput());
+        if (consecutiveNumbers != NaN) { // if consecutiveNumbers is a number 
+            consecutiveNumbers = consecutiveNumbers+this.innerText;
+            printOutput(consecutiveNumbers)
+        }
+    })
+}
 
